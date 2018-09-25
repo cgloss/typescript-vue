daba50c9402ac34954a115f3b3e085799126338e
cgloss <cgloss1@gmail.com>
2018-09-25 13:23:29 -0400

    initial commit
    example typescript / single file component with decorators

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// src/index.ts
+
+import Vue from "vue";
+// import HelloComponent from "./components/Hello";
+// single file component
+import HelloComponent from "./components/Hello.vue";
+
+
+let v = new Vue({
+    el: "#app",
+    template: `
+    <div>
+        Name: <input v-model="name" type="text">
+        <hello-component :name="name" :initialEnthusiasm="5" />
+    </div>
+    `,
+    data: { name: "World" },
+    components: {
+        HelloComponent
+    }
+});
